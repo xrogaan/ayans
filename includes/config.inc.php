583b1d31a8a37a8daf071669ba672e1b824732ed
xrogaan <guru@xrogaan.be>
2010-03-13 08:59:44 +0100

    modify the DEBUG constant name

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -1,12 +1,16 @@
 <?php
 
-error_reporting(E_ALL);
-
-define('DEBUG',true);
+/**
+ * For production mode set the value to 'production'
+ */
+define('APPLICATION_ENVIRONMENT', 'development');
 
 // default sha1 : test
 define('PASSWORD', 'a94a8fe5ccb19ba61c4c0873d391e987982fbbd3');
 
+/**
+ * Config line to pass to PDO
+ */
 define('DBH', 'sqlite:'.dirname(__FILE__).'/../news.sq3');
 
 define('CACHE_PATH','cache/');
