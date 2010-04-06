69d870d19e5d7844da1d05ee48227fb32250eebc
xrogaan <guru@xrogaan.be>
2010-04-06 19:41:21 +0200

    Better dealing with exception in edit.php

--- edit.php
+++ edit.php
@@ -49,8 +49,27 @@ try {
     
     $tpl->input_title = $data['title'];
     $tpl->input_text = $data['text'];
-} catch (PDOException $e) {
-    die("pdo: ".$e->getMessage());
+} catch (Exception $exception) {
+    echo '<html><body><center>'
+       . 'An exception occured while bootstrapping the application.';
+    if (defined('APPLICATION_ENVIRONMENT')
+        && APPLICATION_ENVIRONMENT != 'production'
+    ) {
+        do {
+            $errornot = Services_ErrorNot::getInstance();
+            $errornot->notifyException($exception);
+            echo '<br /><br />' . $exception->getMessage() . '<br />'
+               . '<div align="left">Stack Trace:'
+               . '<pre>' . $exception->getTraceAsString() . '</pre></div>';
+               if ($exception instanceof PDOException) {
+                    $trace = $exception->getTrace();
+                    echo '<div align="left">Query Trace:'
+                    . '<pre>' . $trace[0]['args'][0] . '</pre></div>';
+            }
+        } while($exception = $exception->getPrevious());
+    }
+    echo '</center></body></html>';
+    exit(1);
 }
 
 define('ON_EDIT', true);
