48793715402fdac44eda5bec9c355caf24aff6c5
xrogaan <guru@xrogaan.be>
2010-04-18 06:29:11 +0200

    Delete now delete news (was broken)

--- delete.php
+++ delete.php
@@ -7,30 +7,33 @@
 require 'includes/config.inc.php';
 require INCLUDES_PATH.'prepend.php';
 
-if (empty($_GET['id'])) {
-	die('id is needed');
+if (isset($_POST['id'])) {
+    $id = intval($_POST['id']);
+} elseif (isset($_GET['id'])) {
+    $id = intval($_GET['id']);
+} else {
+    die('id is needed');
 }
-
-$id = intval($_GET['id']);
-
+$text = '';
 if (isset($_POST['password']) && PASSWORD != sha1($_POST['password'])) {
 	header('Location: index.php');
 } elseif (isset($_POST['password']) && PASSWORD == sha1($_POST['password'])) {
 	$pdo = new PDO(DBH);
 
-    $dq = $pdo->query('SELECT COUNT(*) FROM news WHERE id='.$pdo->quote($id),PDO::FETCH_UNIQUE);
+    $dq = $pdo->query('SELECT COUNT(*) FROM news WHERE id='.$pdo->quote($id) ,PDO::FETCH_UNIQUE);
     $re = $dq->fetch();
     $re = array_shift($re);
     if ($re == 1) {
-        $pdo->prepare('DELETE FROM news WHERE id=?');
-        $q = $dq->execute(array(intval($_POST['id'])));
+        $q = $pdo->query('DELETE FROM news WHERE id='. $pdo->quote($id, PDO::PARAM_INT));
         if (!$q) {
-            $text = "The news is not deleted : " . $pdo->errorInfo();
+            $text = "The news is not deleted : " . implode(',', array($pdo->errorInfo()));
         } else {
             $text = "The news $id is successfully deleted.<br/><a href=\"index\">Back</a>";
             unlink('cache/n-'.$id);
             unlink('cache/n-'.$id.'.minimal');
         }
+    } else {
+        die('error');
     }
 }
 ?>
@@ -40,6 +43,9 @@ if (isset($_POST['password']) && PASSWORD != sha1($_POST['password'])) {
 <title>Delete a news</title>
 </head>
 <body>
+<p>
+<?php echo $text ?>
+</p>
 <form action="delete.php" method="post">
 <input type="hidden" name="id" value="<?php echo $id ?>" />
 <p>
